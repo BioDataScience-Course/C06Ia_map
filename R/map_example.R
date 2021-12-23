553e77e385995e912cccaa1b0197ff9c273c0176
phgrosjean <phgrosjean@sciviews.org>
2021-12-23 10:57:51 +0100

    map_example.R reworked to use st_join()

--- R/map_example.R
+++ R/map_example.R
@@ -7,60 +7,83 @@ library(tmap)
 
 # Importation des données
 be <- st_read("data/map_belgium/belgium.shp")
-class(be)
+class(be) # Déjà un objet sf
 
 bee <- read("data/wild_bees_belgium.rds")
-class(bee)
+class(bee) # Déjà un objet sf
 
 # Première carte (pas très lisible)
 tm_shape(be) +
   tm_borders() +
+  # Superposition des points issus du tableau bee
   tm_shape(bee) +
   tm_dots()
-
-# Il est intéressant de voir que certains points sont dans la mer du nord ;)
+# Il est intéressant de noter  que certains points sont dans la Mer du Nord ;)
 # Sacré Bombus !
+
+# Variables à disposition
 str(be)
-st_crs(be)
 str(bee)
+
+# Systèmes de coordonnées géographiques utilisés
+st_crs(be)
 st_crs(bee)
+# WGS 84 dans les deux cas, on est bon, pas besoin de transformer
 
-# Ces jeux de données n'ont pas de variables qui permettent de faire le liens
-# Les localités employées dans le jeu de données bee ne correspondent pas à be
-# On va donc utiliser la fonction st_intersect() qui va lier les deux tableaux
-inter <- st_intersects(be, bee)
-be$nbpts <- sapply(X = inter, FUN = length)
+# Nous voulons placer les observations dans les polygones des communes belges.
+# Le lien entre les deux tableaux peut être fait via les coordonnées
+# géographiques (en déterminant dans quelle commune chaque observation a été
+# faite). Attention que des observations proches de frontières peuvent être
+# attribuées à une autre commune, ou même hors territoire belge, à cause des
+# approximations introduites dans l'arrondi des coordonnées des points de bee !
+bee2 <- st_join(be, bee, left = TRUE)
+# Avec left = TRUE, on est certain de garder toutes les communes, même s'il n'y
+# a aucune observation dedans. Par contre, on perd les points hors territoire
+# belge.
 
-# NISCode est un code pour reconnaître chaque commune, province, région belge
-names(inter) <- be$NISCode
-tab <- tibble(NISCode = names(inter[2]), nrow = inter[[2]])
-for (i in 3:length(inter)) {
-  if (length(inter[[i]]) != 0) {
-    res <- tibble(NISCode = names(inter[i]), nrow = inter[[i]])
-    tab <- bind_rows(tab, res)
-  }
-}
-
-bee$nrow <- 1:nrow(bee)
-bee <- left_join(tab, bee, by = "nrow")
-
-# On s'intéresse aux Megachilidae, par exemple
-bee %>.%
-  filter(., family ==  "Megachilidae") %>.%
-  group_by(., NISCode)%>.%
-  summarise(., tot = length(individualCount)) ->
-  mega
-
-be_mega <- left_join(be, mega, by = "NISCode")
+nrow(bee)
+nrow(bee2)
+# Plus de points après qu'avant ? Attention: une observation sur un frontière de
+# commune sera compté deux fois (dans les deux communes). Il faut filtrer pour
+# éliminer les entrées dupliquées. Nous pouvons nous baser ici sur recordNumber.
+bee2 <- filter(bee2, !duplicated(recordNumber))
+nrow(bee2)
+# Il nous reste donc 13090 observations.
 
 # Cartographie des Megachilidae
-mega_map <- tm_shape(be_mega) +
+# Ici, on va retravailler le jeu de données comme suit:
+# - On ne récupère que les entrées relatives aux Megachilidae
+# - On résume le tableau en dénombrant le nombre d'observations par commune
+#   en regroupant par NISCode
+bee2 %>.%
+  filter(., family ==  "Megachilidae") %>.%
+  group_by(., NISCode) %>.%
+  summarise(., observations = n()) ->
+  mega
+
+tm_shape(mega) +
   tm_borders() +
   tm_compass() +
   tm_scale_bar(position = c("left", "bottom")) +
-  tm_fill("tot",title = "Occurence") +
+  tm_fill("observations", title = "Observations") +
   tm_legend(position = c(0.05, 0.2))
+# Ici, la carte est incomplète car il manque les communes sans observations
 
+# Nous allons donc créer un fond de carte avec les données du tableau be et
+# superposer ensuite les observations issues du tableau mega.
+tm_shape(be) +
+  tm_borders() +
+  tm_fill("gray") +
+  # Seconde couche: on colorie les communes en fonction des observations de mega
+  tm_shape(mega) +
+  tm_borders() +
+  tm_fill("observations", title = "Observations") +
+  # Arrangement des annotations de cartes
+  tm_legend(position = c(0.05, 0.2)) +
+  tm_compass() +
+  tm_scale_bar(position = c("left", "bottom")) ->
+  mega_map
+# Affichage de la carte
 mega_map
 
 # Sauvegarde pour inclure la carte exemple dans README.md sans la recalculer
